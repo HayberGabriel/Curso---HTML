bdb67a98346e364669984e2b96969831dbdbfab9
Hayber Gabriel Rabelo <hayber.rabelo@sefin.fortaleza.ce.gov.br>
2022-05-03 11:37:28 -0300

    Aula 2 finalizada

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert("oi");
+function carregar() {
+    var img = window.document.getElementById('imagem')
+    var msg = window.document.getElementById('msg')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora =22
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA! 
+        img.src = 'imgs/manhã.png'
+        document.body.style.background = '#e2cd9f'
+
+    } else if (hora > 12 && hora <= 18) {
+        // BOA TARDE!
+        img.src = 'imgs/tarde.png'
+        document.body.style.background = '#b9846f'
+
+    } else {
+        // BOA NOITE!
+        img.src = 'imgs/noite.png'
+        document.body.style.background = '#515154'
+    }
+}
